18a4add1002ed48003e37ef1d9c112488bd54e8d
彭富富 <pengff@bwoil.com>
2024-09-10 15:02:24 +0800

    ✨ feat:(custom): test cz

--- src/views/home-view.vue
+++ src/views/home-view.vue
@@ -3,19 +3,16 @@
     <p @click="changeCounter">{{ counter }}</p>
     <p class="primary-text">{{ primary }}</p>
     <p v-hightLight="{ hText: 'module', text: _primary, color: 'blue' }"></p>
-    <p class="warning-text">{{ hh }}</p>
   </div>
 </template>
 
 <script lang="ts" setup>
   import { storeToRefs } from 'pinia'
   import { main, moudule1Store, moudule2Store } from '@/store'
-  import { useDemo } from '@/hooks/useDemo'
   const useMain = main()
   const { counter } = storeToRefs(useMain)
   const { primary } = storeToRefs(moudule1Store())
   const { _primary } = storeToRefs(moudule2Store())
-  const { hh } = useDemo()
   const changeCounter = () => {
     useMain.increment()
   }
